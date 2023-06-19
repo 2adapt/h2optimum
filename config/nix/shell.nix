079cd276181a7a64838a7d93ea645a6cabdbb518
Paulo Vieira <paulovieira@gmail.com>
2023-06-19 10:24:46 +0100

    add configurations for deplyment

--- config/nix/shell.nix
+++ config/nix/shell.nix
@@ -1,24 +1,25 @@
+# if this file is edited using VS Code in windows, it might be necessary to 
+# remove the \r characters:
+# 
+# sed -i 's/\r$//' config/nix/shell.nix
+#
+
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     nodejs-18_x
-
-    # install gdal cli tools such as ogr2ogr, etc
-    #(pkgs.python3.withPackages(p: [p.numpy p.gdal]))
-    
-    #mapserver
-    #curl
-    #wget
   ];
 
-  #shellHook = ''
+  shellHook = ''
     # Mark variables which are modified or created for export.
-    #set -a
-    #source ${toString ../env.sh}
-    #set +a
-    #alias npm="echo npm is not available in this shell. Use pnpm instead."
+    set -a
+    #source env.sh
+    # or to make it relative to the directory of this shell.nix file
+    source ${toString ../env.sh}
+    set +a
+    alias npm="echo npm is not available in this shell. Use pnpm instead."
 
-  #'';
+  '';
 
 }
--- packages/webapp/svelte.config.js
+++ packages/webapp/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,9 +8,35 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		
+		//adapter: adapter()
+
+		adapter: adapter({
+			envPrefix: 'H2OPTIMUM_V2_',
+		}),
+		csrf: {
+			checkOrigin: false,
+			// CSRF should corrected later
+			// https://web.archive.org/web/20230311175338/https://webmatrices.com/d/1623-cross-site-post-form-submissions-are-forbidden-sveltekit/2
+			// https://kit.svelte.dev/docs/adapter-node#environment-variables
+			// https://github.com/sveltejs/kit/issues/6589
+		},
+		typescript: {
+			config: (config) => {
+
+				config.compilerOptions.sourceMap = false;
+				config.compilerOptions.inlineSourceMap = false;
+
+				return config;
+			}
+		}
 	},
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+	compilerOptions: {
+		preserveComments: true,
+		preserveWhitespace: true,
+		enableSourcemap: false
+	}
 };
 
 export default config;
