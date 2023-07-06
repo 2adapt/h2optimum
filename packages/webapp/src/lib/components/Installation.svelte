53ee1cda3623b95e8dfdba1970044aa354d6a2c2
ruipns <ruipns@live.com.pt>
2023-07-06 12:34:05 +0100

    Diag show m

--- packages/webapp/src/lib/components/Installation.svelte
+++ packages/webapp/src/lib/components/Installation.svelte
@@ -22,7 +22,9 @@
 
 
 	let CardGraphTempComp;
-	let dropdownNewDeviceIsOpen = false;
+	let diagIcon;
+	let diagDesc;
+	let showingDiag = false;
 	let dropdownHumIsOpen = false;
 	let isInstallationOwner = false;
 	let urlArray = $page.url.pathname.split('/');
@@ -65,7 +67,23 @@
 
 	data.installation.graph = true;
 
+	function showTitle() {
+		if(showingDiag == false){
+			if(diagIcon.title){
+				diagDesc.innerHTML = diagIcon.title;
+				diagDesc.classList.remove('hidden');
+				showingDiag = true;
+			}
+		} else {
+			diagDesc.classList.add('hidden');
+			showingDiag = false;
+		}
+	}
+
+
+
 </script>
+
 
 
 <!--* SUMMARY + DEVICES -->
@@ -108,13 +126,14 @@
 						Diagnóstico:
 					</div>
 				</div>
-				<div class="sm:flex sm:flex-col sm:items-end">
+				<div  class="sm:flex sm:flex-col sm:items-end relative">
 					{#if Object.keys(errorsObj).length > 0}
-						<span  title="{JSON.stringify(errorsObj)}">
+						<span on:click="{showTitle}" bind:this="{diagIcon}" title="{JSON.stringify(errorsObj)}">
 							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 								<path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
 							</svg>
 						</span>
+						<span bind:this="{diagDesc}" class="hidden absolute right-0 top-7 z-10 w-52 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none "> </span>
 					{:else}
 						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
