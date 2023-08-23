9dffe752869284f0bc2bd2a079b5c9b714da5f21
ruipns <ruipns@live.com.pt>
2023-08-23 12:40:15 +0100

    Fix login

--- packages/webapp/src/routes/login/+page.svelte
+++ packages/webapp/src/routes/login/+page.svelte
@@ -56,14 +56,14 @@
 	<div class="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
 	  <div class="mx-auto w-full max-w-sm lg:w-96">
 		<div>
-		  <h2 class="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+		  <h2 class="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">Inicio de sessão</h2>
 		</div>
   
 		<div class="mt-10">
 		  <div>
 			<form method="POST" class="space-y-6">
 			  <div>
-				<label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+				<label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email é obrigatório</label>
 				<div class="mt-2">
 				  <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
 				</div>
@@ -71,13 +71,13 @@
 			  </div>
   
 			  <div>
-				<label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+				<label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password é obrigatória</label>
 				<div class="mt-2">
 				  <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
 				</div>
 			  </div>
   
-			  <div class="flex items-center justify-between">
+			  <!--<div class="flex items-center justify-between">
 				<div class="flex items-center">
 				  <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
 				  <label for="remember-me" class="ml-3 block text-sm leading-6 text-gray-700">Remember me</label>
@@ -86,7 +86,7 @@
 				<div class="text-sm leading-6">
 				  <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
 				</div>
-			  </div>
+			  </div>-->
   
 			  <div>
 				<button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
