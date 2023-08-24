c15ce9978eefbc1be12f40078c2eff91a445345a
ruipns <ruipns@live.com.pt>
2023-08-24 11:49:22 +0100

    Login2

--- packages/webapp/src/hooks.server.js
+++ packages/webapp/src/hooks.server.js
@@ -4,7 +4,7 @@ import { API_ORIGIN } from '$env/static/private';
 export async function handle({ event, resolve }) {
 	const sidValue = event.cookies.get('sid');
 
-	console.log({ sidValue })
+	//console.log({ sidValue })
 
 	const apiRes = await fetch(`${API_ORIGIN}/api/v2/auth/who`, {
 		method: 'GET',
@@ -14,17 +14,13 @@ export async function handle({ event, resolve }) {
 	});
 
 	const apiData = await apiRes.json();
-	console.log({ apiData })
+	//console.log({ apiData })
 	event.locals.auth = apiData;
 
-	console.log('Antes');	
-	console.log(event.route);
 
 	if(event.route.id && event.route.id.startsWith('/backoffice')){
 		redirectIfNotAuth(event);
 	}
-	console.log('Depois');
-
 
 	return await resolve(event);
 }
